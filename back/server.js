7874b0e417856a0199a11817866091465d9e17f9
vinicius <vin123@gmail.com>
2025-07-14 13:39:23 -0400

    test

--- back/server.js
+++ back/server.js
@@ -99,6 +99,10 @@ Player 4: ${player4name}
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
