f2c75969f02d789874e54e8733488fa10d284616
vinicius <vin123@gmail.com>
2025-07-14 14:01:16 -0400

    commit

--- back/server.js
+++ back/server.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const transport = nodemailer.createTransport({
   service: 'gmail',
